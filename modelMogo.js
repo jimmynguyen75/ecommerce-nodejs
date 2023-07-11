92dec6fba1c5063dfeb67994e6d3f6e8be4c0b2c
Jimmy <minhthusdb@gmail.com>
2023-07-11 14:15:53 +0700

    fixe buffer

--- modelMogo.js
+++ modelMogo.js
@@ -4,6 +4,8 @@ const { connection, connectionIU } = require("./connMogo.js");
 const requestSchema = mongoose.Schema({
   email: String,
   name: String,
+}, {
+  bufferCommands: false,
 });
 
 const reviewSchema = new mongoose.Schema({
@@ -19,11 +21,13 @@ const reviewSchema = new mongoose.Schema({
   size: Number,
   age: String,
   review_date: String,
+}, {
+  bufferCommands: false,
 })
 // const IUDB = mongoose.connection.useDb("iu_csdldn")
 // const NormalDB = mongoose.connection.useDb("nodejs")
-const Request = connection.model("requests", requestSchema);
-const Review = connectionIU.model("review_rating", reviewSchema);
+const Request =  connection.model("requests", requestSchema);
+const Review  =  connectionIU.model("review_rating", reviewSchema);
 
 
 module.exports = {
