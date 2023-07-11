737922f4f5231c999dcc4f61fcbc36bea4c29901
Jimmy <minhthusdb@gmail.com>
2023-07-11 14:04:42 +0700

    update async

--- connMogo.js
+++ connMogo.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 
-const connection = mongoose.createConnection("mongodb+srv://admin:wXI9w0fOSVO7AqBt@cluster0.g561dup.mongodb.net/nodejs?retryWrites=true&w=majority");
-const connectionIU = mongoose.createConnection("mongodb+srv://admin:wXI9w0fOSVO7AqBt@cluster0.g561dup.mongodb.net/iu_csdldn?retryWrites=true&w=majority");
+const connection = async() => {
+    await mongoose.createConnection("mongodb+srv://admin:wXI9w0fOSVO7AqBt@cluster0.g561dup.mongodb.net/nodejs?retryWrites=true&w=majority");
+}
+const connectionIU = async() => {
+    await mongoose.createConnection("mongodb+srv://admin:wXI9w0fOSVO7AqBt@cluster0.g561dup.mongodb.net/iu_csdldn?retryWrites=true&w=majority");
+}
 module.exports= {connection, connectionIU};
